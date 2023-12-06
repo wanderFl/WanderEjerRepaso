85822f34a5a788446cc30e940f95af6ad34b7cb6
Wanderley Paul Flores Proaño <wanderfhp@hotmail.com>
2023-12-05 21:47:42 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WanderEjerRepaso;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WanderEjerRepaso;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
